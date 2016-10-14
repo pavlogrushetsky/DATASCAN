c386c40ee0bd6cf50a4585119365c99556ecbd3c
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2016-10-14 20:25:15 +0300

    Added TcpIpClient

---
+++ DATASCAN/Connections/Common/Crc16.cs
@@ -0,0 +1,42 @@
+﻿namespace DATASCAN.Connections.Common
+{
+    public static class Crc16
+    {
+        const ushort polynomial = 0xA001;
+
+        static readonly ushort[] lookupTable = new ushort[256];
+
+        public static ushort Compute(byte[] bytes)
+        {
+            ushort crc = 0;
+            foreach (byte t in bytes)
+            {
+                byte index = (byte)(crc ^ t);
+                crc = (ushort)((crc >> 8) ^ lookupTable[index]);
+            }
+            return crc;
+        }
+
+        static Crc16()
+        {
+            for (ushort i = 0; i < lookupTable.Length; ++i)
+            {
+                ushort value = 0;
+                var temp = i;
+                for (byte j = 0; j < 8; ++j)
+                {
+                    if (((value ^ temp) & 0x0001) != 0)
+                    {
+                        value = (ushort)((value >> 1) ^ polynomial);
+                    }
+                    else
+                    {
+                        value >>= 1;
+                    }
+                    temp >>= 1;
+                }
+                lookupTable[i] = value;
+            }
+        }
+    }
+}
---
+++ DATASCAN/Connections/TcpIp/TcpIpClient.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Sockets;
+
+namespace DATASCAN.Connections.TcpIp
+{
+    /// <summary>
+    /// Клиент для соединения с вычислителем ROC809
+    /// </summary>
+    public class TcpIpClient
+    {
+        #region Конструктор и поля
+
+        private readonly TcpClient _client;
+        private NetworkStream stream;
+
+        private readonly string _ip;
+        private readonly int _port;
+
+        /// <summary>
+        /// Клиент для соединения с вычислителем ROC809
+        /// </summary>
+        /// <param name="ip">Ip-адрес</param>
+        /// <param name="port">Порт</param>
+        public TcpIpClient(string ip, int port)
+        {
+            // Инициализация полей
+            _ip = ip;
+            _port = port;
+            _client = new TcpClient();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Получение данных из вычислителя ROC809
+        /// </summary>
+        /// <param name="request">Массив байтов запроса</param>
+        /// <returns>Массив байтов ответа</returns>
+        public byte[] GetData(byte[] request)
+        {
+            // Если соединение не было установлено, то установить
+            if (!_client.Connected)
+                _client.Connect(_ip, _port);
+
+            // Получение потока
+            stream = _client.GetStream();
+
+            // Запись запроса в поток
+            stream.Write(request, 0, request.Length);
+            stream.Flush();
+
+            // Чтение ответа из потока
+            byte[] response = new byte[1024];
+            stream.Read(response, 0, response.Length);
+
+            return response;
+        }
+    }
+}
