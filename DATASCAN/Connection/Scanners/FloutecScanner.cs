66348f2b040f6cc8ec35e58f2d3f163829928f3a
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2016-10-24 19:49:56 +0300

    Implemented service and scanner for floutecs

--- DATASCAN/Connection/Scanners/FloutecScanner.cs
+++ DATASCAN/Connection/Scanners/FloutecScanner.cs
@@ -128,9 +128,31 @@ namespace DATASCAN.Connection.Scanners
 
         private async void ScanAlarmData(Floutec floutec, FloutecMeasureLine line)
         {
-            await _dbfService.GetAlarmData(floutec.Address, line.Number, data =>
+            await _dbfService.GetAlarmData(floutec.Address, line.Number, async data =>
             {
-                
+                if (data == null || !data.Any())
+                {
+                    Logger.Log(_log, new LogEntry { Message = $"Дані аварій нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address} відсутні", Status = LogStatus.Warning, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                    return;
+                }
+
+                data.ForEach(d =>
+                {
+                    d.N_FLONIT = floutec.Address*10 + line.Number;
+                    d.FloutecMeasureLineId = line.Id;
+                });
+
+                await _dataService.SaveAlarmData(line.Id, data, saved =>
+                {
+                    Logger.Log(_log,
+                        saved > 0
+                            ? new LogEntry { Message = $"Дані аварій нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address} успішно оновлено. Додано записів: {saved}", Status = LogStatus.Success, Type = LogType.Floutec, Timestamp = DateTime.Now }
+                            : new LogEntry { Message = $"Нові дані аварій нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address} відсутні", Status = LogStatus.Success, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                }, ex =>
+                {
+                    Logger.Log(_log, new LogEntry { Message = $"Помилка збереження даних аварій нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address}", Status = LogStatus.Error, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                    Logger.Log(_log, new LogEntry { Message = ex.Message, Status = LogStatus.Error, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                });
             }, ex =>
             {
                 Logger.Log(_log, new LogEntry { Message = $"Помилка читання даних аварій нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address}", Status = LogStatus.Error, Type = LogType.Floutec, Timestamp = DateTime.Now });
@@ -140,9 +162,31 @@ namespace DATASCAN.Connection.Scanners
 
         private async void ScanHourlyData(Floutec floutec, FloutecMeasureLine line)
         {
-            await _dbfService.GetHourlyData(floutec.Address, line.Number, data =>
+            await _dbfService.GetHourlyData(floutec.Address, line.Number, async data =>
             {
-                
+                if (data == null || !data.Any())
+                {
+                    Logger.Log(_log, new LogEntry { Message = $"Годинні дані нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address} відсутні", Status = LogStatus.Warning, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                    return;
+                }
+
+                data.ForEach(d =>
+                {
+                    d.N_FLONIT = floutec.Address * 10 + line.Number;
+                    d.FloutecMeasureLineId = line.Id;
+                });
+
+                await _dataService.SaveHourlyData(line.Id, data, saved =>
+                {
+                    Logger.Log(_log,
+                        saved > 0
+                            ? new LogEntry { Message = $"Годинні дані нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address} успішно оновлено. Додано записів: {saved}", Status = LogStatus.Success, Type = LogType.Floutec, Timestamp = DateTime.Now }
+                            : new LogEntry { Message = $"Нові годинні дані нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address} відсутні", Status = LogStatus.Success, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                }, ex =>
+                {
+                    Logger.Log(_log, new LogEntry { Message = $"Помилка збереження даних аварій нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address}", Status = LogStatus.Error, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                    Logger.Log(_log, new LogEntry { Message = ex.Message, Status = LogStatus.Error, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                });
             }, ex =>
             {
                 Logger.Log(_log, new LogEntry { Message = $"Помилка читання годинних даних нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address}", Status = LogStatus.Error, Type = LogType.Floutec, Timestamp = DateTime.Now });
@@ -152,9 +196,27 @@ namespace DATASCAN.Connection.Scanners
 
         private async void ScanInstantData(Floutec floutec, FloutecMeasureLine line)
         {
-            await _dbfService.GetInstantData(floutec.Address, line.Number, data =>
+            await _dbfService.GetInstantData(floutec.Address, line.Number, async data =>
             {
-                
+                if (data == null)
+                {
+                    Logger.Log(_log, new LogEntry { Message = $"Миттєві дані нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address} відсутні", Status = LogStatus.Warning, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                    return;
+                }
+
+                data.N_FLONIT = floutec.Address * 10 + line.Number;
+                data.FloutecMeasureLineId = line.Id;
+
+                await _dataService.SaveInstantData(data, saved =>
+                {
+                    Logger.Log(_log,
+                        saved ? new LogEntry { Message = $"Миттєві дані нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address} успішно оновлено", Status = LogStatus.Success, Type = LogType.Floutec, Timestamp = DateTime.Now }
+                              : new LogEntry { Message = $"Нові миттєві дані нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address} відсутні", Status = LogStatus.Success, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                }, ex =>
+                {
+                    Logger.Log(_log, new LogEntry { Message = $"Помилка збереження миттєвих даних нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address}", Status = LogStatus.Error, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                    Logger.Log(_log, new LogEntry { Message = ex.Message, Status = LogStatus.Error, Type = LogType.Floutec, Timestamp = DateTime.Now });
+                });
             }, ex =>
             {
                 Logger.Log(_log, new LogEntry { Message = $"Помилка читання миттєвих даних нитки №{line.Number} обчислювача ФЛОУТЕК з адресою {floutec.Address}", Status = LogStatus.Error, Type = LogType.Floutec, Timestamp = DateTime.Now });
--- DATASCAN/Services/FloutecDataService.cs
+++ DATASCAN/Services/FloutecDataService.cs
@@ -88,5 +88,109 @@ namespace DATASCAN.Services
                 }
             }, TaskScheduler.FromCurrentSynchronizationContext());
         }
+
+        public async Task SaveAlarmData(int lineId, List<FloutecAlarmData> data, Action<int> onSuccess, Action<Exception> onException)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                using (var repo = new DataRecordRepository<FloutecAlarmData>(_connection))
+                {
+                    var lastData = repo.GetAll()
+                        .Where(d => d.FloutecMeasureLineId == lineId)
+                        .OrderByDescending(o => o.DAT)
+                        .FirstOrDefault();
+
+                    if (lastData != null)
+                    {
+                        var filtered = data.Where(d => d.DAT > lastData.DAT).ToList();
+                        repo.Insert(filtered);
+                        return filtered.Count;
+                    }
+
+                    repo.Insert(data);
+
+                    return data.Count;
+                }
+            }, TaskCreationOptions.LongRunning)
+            .ContinueWith(result =>
+            {
+                if (result.Exception != null)
+                {
+                    onException?.Invoke(result.Exception.InnerException);
+                }
+                else
+                {
+                    onSuccess?.Invoke(result.Result);
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        public async Task SaveHourlyData(int lineId, List<FloutecHourlyData> data, Action<int> onSuccess, Action<Exception> onException)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                using (var repo = new DataRecordRepository<FloutecHourlyData>(_connection))
+                {
+                    var lastData = repo.GetAll()
+                        .Where(d => d.FloutecMeasureLineId == lineId)
+                        .OrderByDescending(o => o.DAT)
+                        .FirstOrDefault();
+
+                    if (lastData != null)
+                    {
+                        var filtered = data.Where(d => d.DAT > lastData.DAT).ToList();
+                        repo.Insert(filtered);
+                        return filtered.Count;
+                    }
+
+                    repo.Insert(data);
+
+                    return data.Count;
+                }
+            }, TaskCreationOptions.LongRunning)
+            .ContinueWith(result =>
+            {
+                if (result.Exception != null)
+                {
+                    onException?.Invoke(result.Exception.InnerException);
+                }
+                else
+                {
+                    onSuccess?.Invoke(result.Result);
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        public async Task SaveInstantData(FloutecInstantData data, Action<bool> onSuccess, Action<Exception> onException)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                using (var repo = new DataRecordRepository<FloutecInstantData>(_connection))
+                {
+                    var lastData = repo.GetAll()
+                        .Where(d => d.FloutecMeasureLineId == data.FloutecMeasureLineId)
+                        .OrderByDescending(o => o.DAT)
+                        .FirstOrDefault();
+
+                    if (lastData != null && lastData.IsEqual(data))
+                        return false;
+
+                    repo.Insert(data);
+
+                    return true;
+                }
+            }, TaskCreationOptions.LongRunning)
+            .ContinueWith(result =>
+            {
+                if (result.Exception != null)
+                {
+                    onException?.Invoke(result.Exception.InnerException);
+                }
+                else
+                {
+                    onSuccess?.Invoke(result.Result);
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
     }
 }
